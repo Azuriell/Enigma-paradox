8c497cff89accdabc5da4e82007e8d4b274c674a
Azuriel <lhuaire.jeremie@gmail.com>
2017-05-15 16:27:36 +0200

    Mise en place de l'affichage des clés

---
+++ score.php
@@ -0,0 +1,19 @@
+<?php
+$a = array('truc'=>array('machin'=>TRUE, 'chose'=>'non'), 'autre'=>array('ah ?'));
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Enigma paradox</title>
+	<meta charset="utf-8"/>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
+</head>
+<body>
+	<header>
+		
+	</header>
+	<section>
+	
+	</section>
+</body>
+</html>
